e64b69b1648b97b1660c588d05e9f5b4a0a6732d
Benjamin <bstavare@calpoly.edu>
2024-10-20 00:28:36 -0700

    functions and some testcases for parts 1-3

--- hw1.py
+++ hw1.py
@@ -2,17 +2,57 @@ import data
 
 # Write your functions for each part in the space below.
 
+
 # Part 1
+######################################################################################################################
+# The vowel_count function will have a parameter of type string, and it will return the number of vowels in the
+# given string using the built-in string function to separate the characters, and a for loop to iterate through each
+# character and check if it is a vowel
+
+def vowel_count(input_string:str) -> int:
+    listed_characters = list(input_string)
+    vowels = ['a', 'e', 'i', 'o', 'u','A','E','I','O','U']
+    v_count = 0
+    for i in listed_characters:
+        if i in vowels:
+            v_count += 1
+    return v_count
 
 
 # Part 2
+######################################################################################################################
+# The short_lists function takes a list of lists of integers and returns a new list of all of the nested lists that are
+# of the length 2
+
+def short_lists(input_list:list[list[int]]) -> list:
+    output_list = []
+    for i in input_list:
+        if len(i) ==  2:
+            output_list.append(i)
+    return output_list
 
 
 # Part 3
+########################################################################################################################
+# ascending_pairs function will take a list of lists, identify all "short lists" (lists of length 2) and it will sort
+# the "short lists" into ascending order
+
+def ascending_pairs(input_list:list[list[int]]) -> list[list[int]]:
+    output_list = []
+    for i in input_list:
+        if len(i) == 2:
+            output_list.append(sorted(i))
+        else:
+            output_list.append(i)
+    return output_list
 
 
 # Part 4
-
+########################################################################################################################
+#
+#
+#
+# #
 
 # Part 5
 
--- hw1_test.py
+++ hw1_test.py
@@ -7,15 +7,78 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
+########################################################################################################################
+    # Testing Vowel Count
+
+    # Test for lowercase vowels
+    def test_vowel_count1(self):
+        function_input = "aeiou"
+        expected = 5
+        actual = hw1.vowel_count(function_input)
+        self.assertEqual(actual, expected)
+    # Test for uppercase vowels
+    def test_vowel_count2(self):
+        function_input = "AEIOU"
+        expected = 5
+        actual = hw1.vowel_count(function_input)
+        self.assertEqual(actual, expected)
+
+    # Test for uppercase and lowercase values in a string with spaces
+    def test_vowel_count3(self):
+        function_input = "Once Upon A Time In A Galaxy FAR FAR AWAY"
+        expected = 15
+        actual = hw1.vowel_count(function_input)
+        self.assertEqual(actual, expected)
 
 
-    # Part 2
+#  Part 2
+#######################################################################################################################
+    #Testing short_lists
+    # test function for one short list
+    def test_short_lists1(self):
+        function_input = [[1,2,3],[4,5,6],[7,8,9],[],[1,2]]
+        expected = [[1,2]]
+        actual = hw1.short_lists(function_input)
+        self.assertEqual(actual, expected)
 
+    # test function for empty list
+    def test_short_lists2(self):
+        function_input = []
+        expected = []
+        actual = hw1.short_lists(function_input)
+        self.assertEqual(actual, expected)
 
+    # test with more than one "short list"
+    def test_short_lists3(self):
+        function_input = [[1,2], [1,1],[2]]
+        expected = [[1,2], [1,1]]
+        actual = hw1.short_lists(function_input)
+        self.assertEqual(actual, expected)
+
+########################################################################################################################
     # Part 3
+    # Testing ascending_pairs
+
+    def test_ascending_pairs1(self):
+        function_input = [[1,2,3],[4,5,6],[7,8,9],[],[1,2]]
+        expected = [[1,2,3],[4,5,6],[7,8,9],[],[1,2]]
+        actual = hw1.ascending_pairs(function_input)
+        self.assertEqual(actual, expected)
+    # Test a few arbitrary lists
+    def test_ascending_pairs2(self):
+        function_input = [[2,1],[1,1],[2],[9009,1000],[10000, 5]]
+        expected = [[1,2],[1,1],[2],[1000, 9009],[5, 10000]]
+        actual = hw1.ascending_pairs(function_input)
+        self.assertEqual(actual, expected)
 
 
-    # Part 4
+
+
+
+
+
+
+        # Part 4
 
 
     # Part 5
